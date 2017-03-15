687381821e13afd22989b12c395adb046a9b3345
Mark Vincze <mvincze@travix.com>
2017-03-15 16:15:10 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Google.Cloud.Storage.V1;
+
+namespace gcpstorage_benchmark
+{
+    class Program
+    {
+        private const string bucketName = "mybucket";
+        private const string fileName = "folder/myfile.txt";
+        private const int runCount = 50;
+
+        static void Main(string[] args)
+        {
+            var client = StorageClient.Create();
+            
+            TestMethod(client);
+            TestMethod(client);
+            TestMethod(client);
+            
+            var measurements = new List<TimeSpan>();
+
+            for(int i = 0; i < runCount; i++)
+            {
+                TestMethod(client, measurements);
+            }
+            
+            foreach(var m in measurements)
+            {
+                System.Console.WriteLine("Duration: {0}ms", m.TotalMilliseconds);
+            }
+            
+            System.Console.WriteLine("Average duration: {0}ms", measurements.Sum(m => m.TotalMilliseconds) / runCount);
+        }
+        
+        static void TestMethod(StorageClient client, List<TimeSpan> measurements = null)
+        {
+            using(var ms = new MemoryStream())
+            {
+                var sw = Stopwatch.StartNew();
+                client.DownloadObject(bucketName, fileName, ms);
+                sw.Stop();
+                
+                if(measurements != null)
+                {
+                    measurements.Add(sw.Elapsed);
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# google-storage-benchmark
-A simple benchmark for downloading a file from Google Cloud Storage
+# Simple Cloud Storage benchmark
+A simple benchmark for downloading a file from Google Cloud Storage with the .NET SDK.
+
+## How to use
+In `Program.cs` replace `bucketName`, `fileName` and `runCount` with your own values, and run the application.
